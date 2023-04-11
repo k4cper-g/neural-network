8c2e880ee7ed68dd354ed7165b087f8a3ffea56f
Kacper Gadomski <93160987+k4cper-g@users.noreply.github.com>
2023-04-11 22:30:15 +0200

    Add files via upload

---
+++ neuronNetwork/src/Dummy.java
@@ -0,0 +1,48 @@
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Dummy {
+    static AtomicInteger nextId = new AtomicInteger();
+    int id;
+    String name;
+    List <Double> vector;
+
+    /**
+     * Two param constructor creates a Dummy instance with a passed name and vector.
+     * Automatically increments dummy id.
+     * @param vector - List of Double type values necessary for operations.
+     * @param name - Name of the dummy.
+     */
+    Dummy(List<Double> vector, String name) {
+        this.id = nextId.incrementAndGet();
+        this.name = name;
+        this.vector = vector;
+    }
+
+    /**
+     * One param constructor creates a Dummy instance only with a passed vector.
+     * Automatically increments dummy id.
+     * @param vector - List of Double type values necessary for operations.
+     */
+    Dummy(List<Double> vector) {
+        this.id = nextId.incrementAndGet();
+        this.name = "test" + id;
+        this.vector = vector;
+    }
+
+    /**
+     * Allows to print the object values easily.
+     * @return - Returns a String.
+     */
+    public String toString() {
+        return this.vector + ":" + this.name + ":" + this.id;
+    }
+
+    /**
+     * Allows to set the objects name.
+     * @param name - String type name.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
